b4cd263f95f51cc6dfebad2bb97ab377204470b0
Superstars111 <79164370+Superstars111@users.noreply.github.com>
2021-09-15 13:28:22 -0700

    Add files via upload

---
+++ madlibs_test.py
@@ -0,0 +1,11 @@
+# Tutorial followed from freeCodeCamp.org
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Another verb: ")
+famous_person = input("Somebody famous: ")
+
+madlib = f"Computer programming is so {adj.lower()}! It makes me so excited all the time because \
+I love to {verb1.lower()}. Stay hydrated and {verb2.lower()} like you are {famous_person.title()}!"
+
+print(madlib)
