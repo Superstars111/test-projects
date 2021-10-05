28f0b918885ed8c6091f65caae14aac8c06347b5
Superstars111 <79164370+Superstars111@users.noreply.github.com>
2021-10-05 11:35:46 -0700

    Added smart computer mode

--- tictactoe/player.py
+++ tictactoe/player.py
@@ -17,6 +17,54 @@ class RandomComputer(Player):
         return square
 
 
+class GeniusComputer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else:
+            square = self.minimax(game, self.letter)["position"]
+        return square
+
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player = "O" if player == "X" else "X"
+
+        if state.winner == other_player:
+            return {"position": None,
+                    "score": 1 * (state.num_empty_squares() + 1) if other_player == max_player
+                    else -1 * (state.num_empty_squares() + 1)
+                    }
+
+        elif not state.empty_squares():
+            return {"position": None, "score": 0}
+
+        if player == max_player:
+            best = {"position": None, "score": -math.inf}
+        else:
+            best = {"position": None, "score": math.inf}
+
+        for possible_move in state.available_moves():
+            # Step 1: try a move
+            state.make_move(possible_move, player)
+            # Step 2: check with minimax
+            sim_score = self.minimax(state, other_player)
+            # Step 3: undo the move
+            state.board[possible_move] = " "
+            state.winner = None
+            sim_score["position"] = possible_move
+            # Step 4: update dictionaries if necessary
+            if player == max_player:  # This maximizes the max player
+                if sim_score["score"] > best["score"]:
+                    best = sim_score
+            else:  # This minimizes the other player
+                if sim_score["score"] < best["score"]:
+                    best = sim_score
+        return best
+
+
 class Human(Player):
     def __init__(self, letter):
         super().__init__(letter)
