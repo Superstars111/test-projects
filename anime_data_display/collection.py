a1f9af5f09deb8f23961159aee17f829546302f7
Superstars111 <jared.superstars111@gmail.com>
2021-12-06 15:32:53 -0800

    Added basic display

--- anime_data_display/collection.py
+++ anime_data_display/collection.py
@@ -1,36 +1,7 @@
 import json
 import requests as rq
 
-
-def collect_seasonal_data(show_id, episodes=0, seasons=1):
-    id_var = {"id": show_id}
-    sequel_id = None
-    query = """query($id: Int){
-                 Media(id: $id, type:ANIME){
-                   relations{
-                     edges{
-                       relationType,
-                       node{
-                         id,
-                         episodes}}}}}"""
-    showData = rq.post(url, json={"query": query, "variables": id_var}).json()["data"]["Media"]
-    for series in showData["relations"]["edges"]:
-        if series["relationType"] == "SEQUEL":
-            episodes += series["node"]["episodes"]
-            seasons += 1
-            sequel_id = series["node"]["id"]
-
-    if sequel_id:
-        episodes, seasons, sequel_id = collect_seasonal_data(sequel_id, episodes=episodes, seasons=seasons)
-
-    return episodes, seasons, sequel_id
-
-
 url = "https://graphql.anilist.co/"
-media_id = input("Please input the show's id number. ")
-
-id_var = {"id": media_id}
-
 query = """query($id: Int){
   Media(id: $id, type: ANIME){
     title {
@@ -40,19 +11,6 @@ query = """query($id: Int){
     },
     description,
     episodes,
-    relations {
-      edges {
-        node {
-          title {
-            romaji,
-            english
-          }
-          id,
-          episodes,
-        },
-        relationType
-      }
-    },
     coverImage {
       extraLarge
       large
@@ -68,17 +26,73 @@ query = """query($id: Int){
   }
 }"""
 
+
+def collect_seasonal_data(show_id, episodes=0, seasons=1):
+    id_var = {"id": show_id}
+    sequel_id = None
+    query = """query($id: Int){
+                 Media(id: $id, type:ANIME){
+                   relations{
+                     edges{
+                       relationType,
+                       node{
+                         id,
+                         episodes}}}}}"""
+    showData = rq.post(url, json={"query": query, "variables": id_var}).json()["data"]["Media"]
+    for series in showData["relations"]["edges"]:
+        if series["relationType"] == "SEQUEL":
+            if series["node"]["episodes"]:
+                episodes += series["node"]["episodes"]
+            seasons += 1
+            sequel_id = series["node"]["id"]
+
+    if sequel_id:
+        episodes, seasons, sequel_id = collect_seasonal_data(sequel_id, episodes=episodes, seasons=seasons)
+
+    return episodes, seasons, sequel_id
+
+
+with open("anime_data.json", "r") as anime_data:
+    data = json.load(anime_data)
+
+retry = True
+while retry:
+    duplicate = False
+    media_id = input("Please input the show's id number. ")
+    for show in data:
+        if media_id == show["id"]:
+            print("We already have that show collected.")
+            duplicate = True
+    if not duplicate:
+        retry = False
+
+id_var = {"id": media_id}
 request = rq.post(url, json={"query": query, "variables": id_var}).json()['data']["Media"]
-
-# for field in request:
-#     print(field, request[field])
-
-# for series in request["relations"]["edges"]:
-#     print(series)
-
 total_episodes, seasons, sequel = collect_seasonal_data(media_id, episodes=request["episodes"])
 
+jared_rating = [0, 0, 0]
+simon_rating = [0, 0, 0]
+kenan_rating = [0, 0, 0]
+
+print(f"The show is {request['title']['romaji']}, or {request['title']['english']}.")
+
+jared_rating[0] = int(input("Please input Jared's show rating from 1-100. "))
+jared_rating[1] = int(input("Please input Jared's pacing rating- -50 is slow, 50 is fast. "))
+jared_rating[2] = int(input("Please input Jared's comedy/drama rating. -50 is dramatic, 50 is funny. "))
+
+if input("Does Simon have available ratings? ").lower() in ("y", "yes"):
+    simon_rating[0] = int(input("Please input Simon's show rating from 1-100. "))
+    simon_rating[1] = int(input("Please input Simon's pacing rating- -50 is slow, 50 is fast. "))
+    simon_rating[2] = int(input("Please input Simon's comedy/drama rating. -50 is dramatic, 50 is funny. "))
+
+if input("Does Kenan have available ratings? ").lower() in ("y", "yes"):
+    kenan_rating[0] = int(input("Please input Kenan's show rating from 1-100. "))
+    kenan_rating[1] = int(input("Please input Kenan's pacing rating- -50 is slow, 50 is fast. "))
+    kenan_rating[2] = int(input("Please input Kenan's comedy/drama rating. -50 is dramatic, 50 is funny. "))
+
+
 entry = {
+    "id": media_id,
     "romajiTitle": request["title"]["romaji"],
     "englishTitle": request["title"]["english"],
     "nativeTitle": request["title"]["native"],
@@ -90,7 +104,13 @@ entry = {
     "coverSmall": request["coverImage"]["medium"],
     "genres": request["genres"],
     "tags": request["tags"],
-    "score": request["averageScore"]
+    "score": request["averageScore"],
+    "jaredScore": jared_rating,
+    "simonScore": simon_rating,
+    "kenanScore": kenan_rating
 }
 
-print(entry)
+data.append(entry)
+
+with open("anime_data.json", "w") as anime_data:
+    json.dump(data, anime_data, indent=4)
--- anime_data_display/display.py
+++ anime_data_display/display.py
@@ -1,11 +1,82 @@
+import json
 import tkinter as tk
 from tkinter import ttk
+
+with open("anime_data.json", "r") as anime_data:
+    library = json.load(anime_data)
+options = []
+option_titles = []
+library_titles = []
+
+for show in library:
+    library_titles.append(show["englishTitle"])
+
+
+def add_options():
+    library_var = tk.StringVar(value=library_titles)
+    options_var = tk.StringVar(value=option_titles)
+
+    dlg_selection = tk.Toplevel(window)
+    dlg_selection.attributes("-topmost")
+    dlg_selection.transient(window)
+    dlg_selection.grab_set()
+    btn_confirm = tk.Button(dlg_selection, text="Confirm choices", command=lambda: dismiss_window(dlg_selection))
+    lbox_library = tk.Listbox(dlg_selection, listvariable=library_var)
+    lbox_selected = tk.Listbox(dlg_selection, listvariable=options_var)
+    btn_add_option = tk.Button(dlg_selection, text="Add", command=lambda: add_to_list(lbox_library, lbox_selected))
+    btn_remove_option = tk.Button(dlg_selection, text="Remove", command=lambda: remove_from_list(options, lbox_selected))
+
+    lbox_library.grid(row=1, column=0, rowspan=4)
+    lbox_selected.grid(row=1, column=2, rowspan=4)
+    btn_add_option.grid(row=2, column=1)
+    btn_remove_option.grid(row=3, column=1)
+    btn_confirm.grid(row=5, column=1)
+    dlg_selection.wait_window()
+
+
+def add_to_list(lbox_out, lbox_in):
+    selected = lbox_out.curselection()[0]
+
+    if library_titles[selected] not in option_titles:
+        options.append(library[selected])
+        option_titles.append(library_titles[selected])
+
+    options_var = tk.StringVar(value=option_titles)
+    lbox_in["listvariable"] = options_var
+
+
+def remove_from_list(options_list, lbox):
+
+    options_list.pop(lbox.curselection()[0])
+    option_titles.pop(lbox.curselection()[0])
+
+    options_list_var = tk.StringVar(value=option_titles)
+    lbox["listvariable"] = options_list_var
+
+
+def dismiss_window(popup):
+    popup.grab_release()
+    popup.destroy()
+    options_var = tk.StringVar(value=option_titles)
+    lbox_options["listvariable"] = options_var
+
+
+def update_page_display(test):
+    show = options[lbox_options.curselection()[0]]
+    seenby = 0
+    for rating in (show["jaredScore"][0], show["simonScore"][0], show["kenanScore"][0]):
+        if rating > 0:
+            seenby += 1
+
+    lbl_title["text"] = f"{show['romajiTitle']} \u2022 {show['englishTitle']} \u2022 {show['nativeTitle']}"
+    lbl_avg_rating["text"] = f"Public rating: {show['score']}/100 \u2605"
+    lbl_house_rating["text"] = f"House rating: {(show['jaredScore'][0] + show['simonScore'][0] + show['kenanScore'][0])//seenby}/100 \u2605"
 
 
 if __name__ == "__main__":
     window = tk.Tk()
     frm_anime_display = tk.Frame(window)
-    lbl_title = tk.Label(frm_anime_display, text="Anime Title Here")
+    lbl_title = tk.Label(frm_anime_display, text="Anime Title Here", font=("Arial", 15))
     lbl_avg_rating = tk.Label(frm_anime_display, text="Avg Rating Here")
     lbl_house_rating = tk.Label(frm_anime_display, text="House Rating Here")
     cnvs_graph = tk.Canvas(frm_anime_display)
@@ -17,9 +88,12 @@ if __name__ == "__main__":
     lbl_spoiler_genres = tk.Label(frm_anime_display, text="Spoiler Genres Hidden")
     lbl_tags = tk.Label(frm_anime_display, text="Tag List Here")
     lbl_spoiler_tags = tk.Label(frm_anime_display, text="Spoiler Tags Hidden")
-    lbox_options = tk.Listbox(window)
+    lbox_options = tk.Listbox(window, height=35)
+    btn_options = tk.Button(window, text="Add Options", command=add_options)
 
-    lbl_title.grid(row=0, column=0, columnspan=4)
+    lbox_options.bind("<<ListboxSelect>>", update_page_display)
+
+    lbl_title.grid(row=0, column=0, columnspan=4, pady=8)
     lbl_avg_rating.grid(row=1, column=0)
     lbl_house_rating.grid(row=1, column=1)
     cnvs_graph.grid(row=2, column=0, rowspan=3, columnspan=2)
@@ -33,5 +107,7 @@ if __name__ == "__main__":
     lbl_spoiler_tags.grid(row=8, column=0)
 
     frm_anime_display.grid()
+    lbox_options.grid(row=0, column=1)
+    btn_options.grid(row=1, column=1)
 
     window.mainloop()
